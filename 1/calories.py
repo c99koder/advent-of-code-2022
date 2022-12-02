4872faf75b11b25127f0c203e3bc68e0510fee35
Sam Steele <sam.steele@gmail.com>
2022-12-02 14:13:43 -0500

    initial commit

---
+++ 1/calories.py
@@ -0,0 +1,30 @@
+import sys, operator
+
+currentElf = 0
+currentTotal = 0
+
+totals = []
+
+print('--- Day 1: Calorie Counting ---')
+for line in open('input.txt', 'r').readlines():
+    line=line.rstrip()
+    if line.isnumeric():
+        currentTotal+=int(line)
+    else:
+        totals.append({'elf': currentElf, 'total': currentTotal})
+        currentElf += 1
+        currentTotal = 0
+
+totals = sorted(totals, key=operator.itemgetter('total'), reverse=True)
+
+count = 0
+currentTotal = 0
+
+print('Elf\tTotal')
+for total in totals:
+    print(f'{total["elf"]}\t{total["total"]}')
+    currentTotal += total["total"]
+    count += 1
+    if (count > 2):
+        break
+print(f'Total:\t{currentTotal}')
---
+++ 2/rock-paper-scissor.py
@@ -0,0 +1,47 @@
+import sys
+
+GUIDE = {'A': 'rock', 'B': 'paper', 'C': 'scissors', 'X': 'rock', 'Y': 'paper', 'Z': 'scissors'}
+SCORES = {'rock': 1, 'paper': 2, 'scissors': 3}
+WINNERS = {'rock': 'scissors', 'paper': 'rock', 'scissors': 'paper'}
+LOSERS = {'rock': 'paper', 'paper': 'scissors', 'scissors': 'rock'}
+
+DRAW_BONUS = 3
+WIN_BONUS = 6
+
+score_part1 = 0
+score_part2 = 0
+
+print('--- Day 2: Rock Paper Scissors ---')
+
+def part1(opponent, player):
+    global score_part1
+
+    score_part1 += SCORES[player]
+
+    if opponent == player:
+        score_part1 += DRAW_BONUS
+    elif opponent == WINNERS[player]:
+        score_part1 += WIN_BONUS
+
+def part2(opponent, outcome):
+    global score_part2
+    if outcome == 'X': #Lose
+        player = WINNERS[opponent]
+    elif outcome == 'Y': #Draw
+        player = opponent
+    elif outcome == 'Z': #Win
+        player = LOSERS[opponent]
+
+    score_part2 += SCORES[player]
+
+    if opponent == player:
+        score_part2 += DRAW_BONUS
+    elif opponent == WINNERS[player]:
+        score_part2 += WIN_BONUS
+
+for line in open('input.txt', 'r').readlines():
+    line = line.rstrip().split()
+    part1(GUIDE[line[0]], GUIDE[line[1]])
+    part2(GUIDE[line[0]], line[1])
+
+print(f'Final scores:\tpart 1: {score_part1}\tpart 2:{score_part2}')
